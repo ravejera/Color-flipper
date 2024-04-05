0cfa9a5bde7001b22a429aca974f2904c854f281
GeraldRavera <raveragerald960@gmail.com>
2024-04-05 18:23:57 +0300

    added full project

---
+++ index.js
@@ -0,0 +1,21 @@
+// const colors = ["red", "blue", "greenyellow", "green", "white"]
+
+
+
+
+// const btn = document.querySelector('.btn')
+// const color = document.querySelector('.color')
+
+// btn.addEventListener("click", () => {
+//     // console.log(document.body);
+//     const randomNumber = getRandomNumber();
+//     // console.log(randomNumber);
+//     document.body.style.backgroundColor = colors[randomNumber];
+//     color.textContent = colors[randomNumber];
+//     color.style.color = colors[randomNumber]
+// })
+
+// const getRandomNumber = () => {
+//     // return Math.random();
+//     return Math.floor((Math.random()* colors.length));
+// }
